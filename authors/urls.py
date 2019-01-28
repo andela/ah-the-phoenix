0fc49b131c49a449a24896b55b48b071c2fb1ae2
[Arusey] <[kevinkibetw7@gmail.com]>
2019-01-28 23:26:35 +0300

    chore(postgresql):Integrate posgressl Database engine - configure postgres database engine - create travis.yml [starts #163388127]

--- authors/urls.py
+++ authors/urls.py
@@ -1,5 +1,4 @@
 """authors URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
@@ -21,4 +20,3 @@ urlpatterns = [
 
     url(r'^api/', include('authors.apps.authentication.urls', namespace='authentication')),
 ]
-
