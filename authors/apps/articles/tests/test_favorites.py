2bbf0ceedaed65b2610bfba907fd00209c01d40d
asirvex <asirvex@gmail.com>
2019-03-22 00:42:00 +0300

    bug(author not included in fav articles) - add authors to favorite articles

--- authors/apps/articles/tests/test_favorites.py
+++ authors/apps/articles/tests/test_favorites.py
@@ -63,9 +63,18 @@ class TestFavoriteArticle(BaseTest):
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, [{
-            'article_slug': 'rate-this',
+            'slug': 'rate-this',
             'title': 'rate this',
             'image': None,
             'description': 'to be used in rating tests',
-            'body': 'whose afraid of the big bad wolf?'
+            'body': 'whose afraid of the big bad wolf?',
+            "author": {
+                "email": "pherndegz@gmail.com",
+                "username": "PaulGichuki",
+                "bio": "",
+                "image": "image/upload/https://res.cloudinary.com/dw675k0f5/image/upload/v1542660993/sample.jpg",              # noqa E501
+                "followers_total": 0,
+                "following_total": 0
+            }
+
         }])
